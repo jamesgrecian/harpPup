15779ce30133ad91672478024819d12bd0f6c3ef
James Grecian <james.grecian@gmail.com>
2020-01-21 12:32:24 +0000

    Ensure SRDL_time uses min and max date for each individual

--- R/SRDL_time.R
+++ R/SRDL_time.R
@@ -9,11 +9,11 @@ SRDL_time <- function(locs){
   
   time <- locs %>%
     group_by(id) %>%
-    summarise(min = floor_date(min(locs$date), "6 hours"),
-              max = ceiling_date(max(locs$date), "6 hours")) %>%
+    summarise(min = floor_date(min(date), "6 hours"),
+              max = ceiling_date(max(date), "6 hours")) %>%
     mutate(date = map2(min, max, seq, by = "6 hours")) %>% # Create a list column with dates
     unnest(cols = c(date)) %>%
     select(id, date)
-  
+
   return(time)
 }
