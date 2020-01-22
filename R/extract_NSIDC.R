688807cbae5c96e812e0e87ffdc3a2e90c17d3b8
James Grecian <james.grecian@gmail.com>
2020-01-22 14:40:51 +0000

    Extracted NSIDC concentration for locations outside raster extent should be set to 0

--- R/extract_NSIDC.R
+++ R/extract_NSIDC.R
@@ -14,6 +14,9 @@ extract_NSIDC <- function(df, path){
   ## map our data to this file-db (we might have multiple points per file)
   df$fullname <- files$fullname[findInterval(df$date, files$date)]
   
+  ## set up progress bar based on number of files that will be loaded
+  pb <- progress_estimated(length(unique(df$fullname)))
+  
   (rdummy <- raster(df$fullname[1]))
   
   ## project the query points
@@ -22,10 +25,13 @@ extract_NSIDC <- function(df, path){
   ## now, extract per file 
   df <- purrr::map_df(split(df, df$fullname)[unique(df$fullname)], 
                       function(.x) {
-                        print(.x$fullname[1])
+                        pb$tick()$print()
                         .x["concentration"] <- raster::extract(raster(.x$fullname[1]), as.matrix(.x[c("X", "Y")]))
                         .x
                       })
+  # if animal locations are further south than ice raster extent set concentration to 0
+  df <- df %>% mutate(concentration = if_else(Y < raster::extent(r)[3], 0, concentration))
+
   # #0 is ocean; 2510 pole hole; 2530 coast line; 2540 land; 2550 missing
   # 0-1000 so divide by 10 to get percentage
   df$concentration[df$concentration > 1000] <- NA
@@ -33,6 +39,7 @@ extract_NSIDC <- function(df, path){
   
   # check order of dataframe
   df <- df %>% arrange(id, date)
+  df <- df %>% dplyr::select(-c("fullname", "X", "Y"))
   
   return(df)
 }
